4a100053f29ab960bbedaccb20c865f80afb2af2
Fraser Wilkinson <fraserwilk@users.noreply.github.com>
2024-03-26 12:00:28 +1100

    ob_end_flush fix

--- functions.php
+++ functions.php
@@ -194,3 +194,13 @@ add_action( 'login_enqueue_scripts', 'wpb_login_logo' );
 // <!--- WordPress Login Logo and URL
 
 
+/**
+ * Proper ob_end_flush() for all levels
+ *
+ * This replaces the WordPress `wp_ob_end_flush_all()` function
+ * with a replacement that doesn't cause PHP notices.
+ */
+remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
+add_action( 'shutdown', function() {
+   while ( @ob_end_flush() );
+} );
